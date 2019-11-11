2c226bfcbaa9f7d665145066828a092456897cc9
ParkChaeLin-818 <qkrcofls6863@gmail.com>
2019-11-11 13:17:45 +0900

    MouseEvent 와 KeyEvent 메소드 작성

--- MyHelloPanelListener.java
+++ MyHelloPanelListener.java
@@ -1,11 +1,31 @@
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 /**
  * 여기에 MyHelloPanelListener 클래스 설명을 작성하십시오.
  * 
- * @author (작성자 이름) 
+ * @author (2018315010,2018315057) 
  * @version (버전번호나 날짜)
  */
-public class MyHelloPanelListener
+public class MyHelloPanelListener extends JPanel
+                        implements MouseListener,keyListener
 {
+    public JPanel panel;
+    public JLabel label;
+    public MyHellpPanelListener (JPanel p,JLanel l)
+    {
+        panel = p;
+        label = l;
+    }
+    public void mouseClicked(MouseEvent e){
+        
+    }
+    public void mouseEntered(MouseEvent e){}
+    public void mouseExited(MouseEvent e){}
+    public void mousePressed(MouseEvent e){}
+    public void mouseReleased(MouseEvent e){}
+    public void keyPressed(keyEvent e){}
+    public void keyReleased(keyEvent e){}
+    public void keyTyped(keyEvent e){}
     
 }

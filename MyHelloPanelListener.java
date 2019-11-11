7dece13dec921a48572e41b24f10b090cb85dc59
keet <wwukuww@gmail.com>
2019-11-11 13:40:47 +0900

    리스너클래스 구현

--- MyHelloPanelListener.java
+++ MyHelloPanelListener.java
@@ -12,9 +12,15 @@ implements MouseListener,keyListener
 {
     public JPanel panel;
     public JLabel label;
-    public MyHellpPanelListener (JPanel p,JLanel l){
+    public MyHellpPanelListener (JPanel p,JLabel l){
         panel = p;
         label = l;
+        l.setSize(100,20);
+        l.setLocation(50,50);
+        this.setLayout(null);
+        l.setFocusable(true);
+        l.requestFocus();
+        this.add(l);
     }
     public void mouseClicked(MouseEvent e){
 
@@ -22,10 +28,24 @@ implements MouseListener,keyListener
     public void mouseEntered(MouseEvent e){}
     public void mouseExited(MouseEvent e){}
     public void mousePressed(MouseEvent e){
-        public void 
+        int x = e.getX();
+        int y = e.getY();
+        l.setLocation(x,y);
     }
     public void mouseReleased(MouseEvent e){}
-    public void keyPressed(keyEvent e){}
+    public void keyPressed(keyEvent e){
+    int keyCode = e.getKeyCode();
+        switch(keyCode){
+            case KeyEvent.VK_UP:
+            l.setLocation(l.getX(), l.getY()-10);break;
+            case KeyEvent.VK_DOWN:
+            l.setLocation(l.getX(), l.getY()+10);break;
+            case KeyEvent.VK_LEFT:
+            l.setLocation(l.getX()-10,l.getY());break;
+            case KeyEvent.VK_RIGHT:
+            l.setLocation(l.getX()+10,l.getY());break;
+        }
+    }
     public void keyReleased(keyEvent e){}
     public void keyTyped(keyEvent e){}
 }
